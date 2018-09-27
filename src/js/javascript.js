1857d662dd7f372024399d0116f7ef902bb20ca7
Mouhcine <Mouhcine@admins-MacBook-Air-3.local>
2018-09-27 15:53:14 +0200

    js mod

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,3 +1,10 @@
+function maFonction(){
+  var elements;
+  elements = document.getElementsByClassName("tout");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none" ;
+  }
+}
 function affiche(id)
 {  var elements;
   elements = document.getElementsByClassName("tout");
@@ -6,7 +13,15 @@ function affiche(id)
     for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = "none" ;
     }
-    document.getElementById(id).style.display="block";
+     setTimeout(function(){ var maDiv=  document.getElementById(id);
+                         maDiv.style.display="block";
+                          },500);
+    /*  var source=document.getElementById(id);
+      var dl=source.getElementsByTagName("DL");
+      var j;
+      for( j=0;j< dl.length ; j++){
+        setTimeout(dl[j].style.display="block",1000); } */
+
   }
   else
   {
