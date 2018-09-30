46b67e5a3eca0a57d7e4d9e973a373a2ffcf4c89
Mouhcine <Mouhcine@admins-MacBook-Air-3.local>
2018-09-30 21:37:59 +0200

    Version_Finale

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,34 +1,102 @@
-function maFonction(){
-  var elements;
-  elements = document.getElementsByClassName("tout");
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.display = "none" ;
-  }
-}
-function affiche(id)
-{  var elements;
-  elements = document.getElementsByClassName("tout");
-  if(document.getElementById(id).style.display==="none")
-  {
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].style.display = "none" ;
-    }
-     setTimeout(function(){ var maDiv=  document.getElementById(id);
-                         maDiv.style.display="block";
-                          },500);
-    /*  var source=document.getElementById(id);
-      var dl=source.getElementsByTagName("DL");
-      var j;
-      for( j=0;j< dl.length ; j++){
-        setTimeout(dl[j].style.display="block",1000); } */
+var collapsesHTML = document.getElementsByClassName("collapse");
+var collapses = [];
+var displayed = null;
 
-  }
-  else
-  {
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].style.display = "none" ;
+var handleClick = function(collapse) {
+  var hidden = collapse.getAttribute("class").indexOf("plus") !== -1;
+  var targetId = collapse.getAttribute("href");
+  var target = document.getElementById(targetId);
+  if (hidden) {
+    collapse.className = "far fa-minus-square collapse";
+    target.className = "subsection-content display";
+    if (displayed) {
+      displayed.collapse.className = "far fa-plus-square collapse";
+      displayed.element.className = "subsection-content hide";
     }
-    document.getElementById(id).style.display="none";
+    displayed = {
+      collapse: collapse,
+      element: target
+    };
+    target.style.transform = "scaleY(0)";
+    var scaleY = 0.0;
+    var time = setInterval(frame, 100);
+    function frame() {
+      if (scaleY >= 0.95) {
+        scaleY = 1.0;
+        target.style.transform = "scaleY(" + scaleY + ")";
+        clearInterval(time);
+      } else {
+        scaleY += 0.14;
+        target.style.transform = "scaleY(" + scaleY + ")";
+      }
+    }
+  } else {
+    collapse.className = "far fa-plus-square collapse";
+    target.className = "subsection-content hide";
+    displayed = null;
   }
-  return true;
+};
+
+for (var i = 0; i < collapsesHTML.length; i++) {
+  collapses = [...collapses, collapsesHTML.item(i)];
+}
+
+collapses.forEach(function(collapse) {
+  collapse.onclick = function() {
+    handleClick(collapse);
+  };
+});
+
+// -------------------------------------------------------------
+
+var tooltipsHTML = document.getElementsByClassName("tooltip");
+var tooltips = [];
+var tooltiptextsHTML = document.getElementsByClassName("tooltiptext");
+var tooltiptexts = [];
+
+for (var i = 0; i < tooltipsHTML.length; i++) {
+  tooltips = [...tooltips, tooltipsHTML.item(i)];
+  tooltiptexts = [...tooltiptexts, tooltiptextsHTML.item(i)];
+}
+
+tooltips.forEach(function(tooltip) {
+  tooltip.onmousemove = function(event) {
+    var rect = tooltip.getBoundingClientRect();
+    var x = event.clientX - rect.x;
+    tooltiptexts.forEach(function(tooltiptext) {
+      tooltiptext.style.left = x + "px";
+    });
+  };
+});
+
+// -------------------------------------------------------------
+
+var levels = {
+  BI: 4,
+  ML: 3,
+  Cloud: 3,
+  SSI: 2,
+  Hadoop: 4,
+  Hive: 3,
+  C: 3,
+  Java: 3,
+  PHP: 4,
+  Matlab: 4,
+  HTML: 4,
+  CSS: 4,
+  Javascript: 4,
+  SQL: 3,
+  PLSQL: 3,
+  R: 4,
+  MYSQL: 4,
+  Oracle: 3
+};
+
+for (field in levels) {
+  var stars = document.getElementsByClassName(field).item(0);
+  var children = stars.children;
+  for (var i = 0; i < levels[field]; i++) {
+    var star = children.item(i);
+    star.className = "fas fa-star";
+  }
 }
